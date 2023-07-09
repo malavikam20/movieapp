debde25227d2bae91295a9ce634c01cc6fc2de0e
Malavika M <109109754+malavikam20@users.noreply.github.com>
2023-07-10 00:47:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=63e6405a"; //API url
+const API_URL = "http://www.omdbapi.com?apikey=API_KEY"; //API url
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState(""); //search term state
