af500dc50a0973bf1572f4f59baaff55de430d03
Malavikam20 <malavikamanoj495@gmail.com>
2023-07-09 19:01:17 +0530

    Updated with comments

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => { // Destructuring props
   return (
-    <div className="movie" key={imdbID}>
+    <div className="movie" key={imdbID}> {/*Key is required when rendering a list of elements */}
       <div>
-        <p>{Year}</p>
+        <p>{Year}</p> 
       </div>
 
       <div>
         <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+        {/* If the poster is not available, use a placeholder image */}
       </div>
 
       <div>
